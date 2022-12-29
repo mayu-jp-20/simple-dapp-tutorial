a6af108dc4c7f055ce6fedc235c41a7ed3d3f005
mayu-jp-20 <mayu-jp-20>
2022-12-29 14:38:52 +0900

    first commit

--- start/contract.js
+++ start/contract.js
@@ -30,9 +30,52 @@ The `piggybankContract` is compiled from:
   }
 */
 
+
 const forwarderOrigin = 'http://localhost:9010'
 
 const initialize = () => {
-  //You will start here 
+  const onboardButton = document.getElementById('connectButton');
+  const getAccountsButton = document.getElementById('getAccounts');
+  const getAccountsResult = document.getElementById('getAccountsResult');
+
+  const isMetaMaskInstalled = () => {
+    const {ethereum} = window;
+    return Boolean(ethereum && ethereum.isMetaMask);
+  }
+
+  const onboarding = new MetaMaskOnboarding({forwarderOrigin});
+
+  const onclickInstall = () => {
+    onboardButton.innerText = 'Onboarding in progress';
+    onboardButton.disabled = true;
+    onboarding.startOnboarding();
+  }
+
+  const onClickConnect = async () => {
+    try{
+      await ethereum.request({method: 'eth_requestAccounts'});
+    }catch(error){
+      console.error(error);
+    }
+  }
+
+  const MetaMaskClientCheck = () => {
+    if(!isMetaMaskInstalled()){
+      onboardButton.innerText = 'Click here to install MetaMask!';
+      onboardButton.onclick = onclickInstall;
+      onboardButton.disabled = false;
+    }else{
+      onboardButton.innerText = 'Connect';
+      onboardButton.onclick = onClickConnect;
+      onboardButton.disabled = false;
+    }
+  };
+
+  getAccountsButton.addEventListener('click',async() => {
+    const accounts = await ethereum.request({method: 'eth_accounts'});
+    getAccountsResult.innerText = accounts[0] || 'Not able to get accounts';
+  })
+
+  MetaMaskClientCheck();
 }
 window.addEventListener('DOMContentLoaded', initialize)
